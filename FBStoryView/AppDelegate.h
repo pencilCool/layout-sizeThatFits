9dbc9d9677a6b30e68f0aefac7259d5e99648022
pencilCool <yhtang_xt@163.com>
2016-11-09 09:43:41 +0800

    Initial Commit

---
+++ FBStoryView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FBStoryView
+//
+//  Created by tangyuhua on 2016/11/9.
+//  Copyright © 2016年 tangyuhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FBStoryView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FBStoryView
+//
+//  Created by tangyuhua on 2016/11/9.
+//  Copyright © 2016年 tangyuhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
