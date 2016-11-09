de0d4e9db91cbc34b9ba110ec2544b5b5d1cee7d
pencilCool <yhtang_xt@163.com>
2016-11-09 11:15:45 +0800

    autolayout ，代码示例， UI没有显示： 约束不够。我们平时都用Masonry来写了。 原生的不方便。

--- FBStoryView/FBStoryView.m
+++ FBStoryView/FBStoryView.m
@@ -32,46 +32,63 @@
         _likeBarView = [[FBLikeBarView alloc] init];
         
         [self setSubViewColor];
-        
-        [self addSubview:_headerView];
-        [self addSubview:_messageView];
-        [self addSubview:_attachmentView];
-        [self addSubview:_likeBarView];
         self.backgroundColor = [UIColor grayColor];
+        
+//        [self addSubview:_headerView];
+//        [self addSubview:_messageView];
+//        [self addSubview:_attachmentView];
+//        [self addSubview:_likeBarView];
+        
+        
+        //  34 行代码版本： https://gist.github.com/adamjernst/2d52beb72506863f0ac5
+        
+        [self setTranslatesAutoresizingMaskIntoConstraints:NO];
+        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_headerView][_messageView][_attachmentView][_likeBarView]|"
+                                                                     options:NSLayoutFormatAlignAllLeft
+                                                                     metrics:@{}
+                                                                       views:NSDictionaryOfVariableBindings(_headerView, _messageView, _attachmentView, _likeBarView)]];
+        for (UIView *view in @[_headerView, _messageView, _attachmentView, _likeBarView]) {
+            [self addSubview:view];
+            [view setTranslatesAutoresizingMaskIntoConstraints:NO];
+            [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|[view]|"
+                                                                         options:NSLayoutFormatAlignAllTop
+                                                                         metrics:@{}
+                                                                           views:@{@"view": view}]];
+        }
     }
     return self;
 }
-
-- (CGSize)sizeThatFits:(CGSize)size
-{
-    // sizeThatFits 的参数 在这里其实就是宽度有显示，高度没有限制。
-    // 不如uilabel 根据内容来调整尺寸，尺寸有很多调整方法，左右拉长和上下拉长。那么到底采用那一中方式来实现呢。
-    
-    return CGSizeMake(size.width,
-                      [_headerView sizeThatFits:CGSizeMake(size.width, CGFLOAT_MAX)].height
-                      + [_messageView sizeThatFits:CGSizeMake(size.width, CGFLOAT_MAX)].height
-                      + [_attachmentView sizeThatFits:CGSizeMake(size.width, CGFLOAT_MAX)].height
-                      + [_likeBarView sizeThatFits:CGSizeMake(size.width, CGFLOAT_MAX)].height);
-}
-
-- (void)layoutSubviews
-{
-    CGFloat width = [self bounds].size.width;
-    CGFloat y = 0;
-    CGFloat headerHeight = [_headerView sizeThatFits:CGSizeMake(width, CGFLOAT_MAX)].height;
-    [_headerView setFrame:CGRectMake(0, y, width, headerHeight)];
-    y += headerHeight;
-    CGFloat messageHeight = [_messageView sizeThatFits:CGSizeMake(width, CGFLOAT_MAX)].height;
-    [_messageView setFrame:CGRectMake(0, y, width, messageHeight)];
-    y += messageHeight;
-    CGFloat attachmentHeight = [_attachmentView sizeThatFits:CGSizeMake(width, CGFLOAT_MAX)].height;
-    [_attachmentView setFrame:CGRectMake(0, y, width, attachmentHeight)];
-    y += attachmentHeight;
-    CGFloat likeBarHeight = [_likeBarView sizeThatFits:CGSizeMake(width, CGFLOAT_MAX)].height;
-    [_likeBarView setFrame:CGRectMake(0, y, width, likeBarHeight)];
-    y += likeBarHeight;
-}
-
+//
+//- (CGSize)sizeThatFits:(CGSize)size
+//{
+//    // sizeThatFits 的参数 在这里其实就是宽度有显示，高度没有限制。
+//    // 不如uilabel 根据内容来调整尺寸，尺寸有很多调整方法，左右拉长和上下拉长。那么到底采用那一中方式来实现呢。
+//    
+//    return CGSizeMake(size.width,
+//                      [_headerView sizeThatFits:CGSizeMake(size.width, CGFLOAT_MAX)].height
+//                      + [_messageView sizeThatFits:CGSizeMake(size.width, CGFLOAT_MAX)].height
+//                      + [_attachmentView sizeThatFits:CGSizeMake(size.width, CGFLOAT_MAX)].height
+//                      + [_likeBarView sizeThatFits:CGSizeMake(size.width, CGFLOAT_MAX)].height);
+//}
+//
+//- (void)layoutSubviews
+//{
+//    CGFloat width = [self bounds].size.width;
+//    CGFloat y = 0;
+//    CGFloat headerHeight = [_headerView sizeThatFits:CGSizeMake(width, CGFLOAT_MAX)].height;
+//    [_headerView setFrame:CGRectMake(0, y, width, headerHeight)];
+//    y += headerHeight;
+//    CGFloat messageHeight = [_messageView sizeThatFits:CGSizeMake(width, CGFLOAT_MAX)].height;
+//    [_messageView setFrame:CGRectMake(0, y, width, messageHeight)];
+//    y += messageHeight;
+//    CGFloat attachmentHeight = [_attachmentView sizeThatFits:CGSizeMake(width, CGFLOAT_MAX)].height;
+//    [_attachmentView setFrame:CGRectMake(0, y, width, attachmentHeight)];
+//    y += attachmentHeight;
+//    CGFloat likeBarHeight = [_likeBarView sizeThatFits:CGSizeMake(width, CGFLOAT_MAX)].height;
+//    [_likeBarView setFrame:CGRectMake(0, y, width, likeBarHeight)];
+//    y += likeBarHeight;
+//}
+//
 
 
 - (void)setSubViewColor{
@@ -80,5 +97,6 @@
     _attachmentView.backgroundColor = [UIColor yellowColor];
     _likeBarView.backgroundColor = [UIColor blueColor];
 }
+
 
 @end
